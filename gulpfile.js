f668295c6bee826524efbcb91c7f8ce87b9dbe89
drewprice <drewsprice@gmail.com>
2017-02-28 20:20:30 -0500

    Mastermindify

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ var buildDir = path.join(__dirname, 'build')
 console.log('build directory', buildDir)
 
 function productName() {
-  var name = 'Learn IDE';
+  var name = 'Mastermind IDE';
 
   if (buildBeta) {
     name += ' Beta';
